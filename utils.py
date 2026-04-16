1de94af3ebf9ad32098f51bf8cb8b37f6741c630
mouldi204-web <mouldi204@gmail.com>
2026-04-16 06:24:52 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
+import requests
+import time
 
+# =========================
+# SAFE REQUEST
+# =========================
+def safe_get(url):
+    try:
+        return requests.get(url).json()
+    except:
+        return None
+
+# =========================
+# BATCH SPLIT
+# =========================
+def chunk(lst, size):
+    for i in range(0, len(lst), size):
+        yield lst[i:i+size]
+
+# =========================
+# SLEEP SAFE
+# =========================
+def safe_sleep(sec):
+    time.sleep(sec)
